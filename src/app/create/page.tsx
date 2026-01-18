2837faf7a8a5acf00cb821ea8e307c03786f35ad
hogehoge315 <yojiro0714@gmail.com>
2026-01-18 15:39:55 +0900

    Chakra UIの設定と新しいページの追加、不要なスタイルの削除

---
+++ src/app/create/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { Box, Container, Heading, Text, VStack } from "@chakra-ui/react";
+import { ColorModeButton } from "@/components/ui/color-mode";
+
+export default function CreatePage() {
+  return <Text>Create Page</Text>;
+}
